333cded502bbaaced59ac00106aaf899e73f77b9
shala190035 <55692386+shala190035@users.noreply.github.com>
2024-01-16 08:54:56 +0100

    read_write_csv

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import json
 
 
 def read_csv(filename):
@@ -7,6 +8,14 @@ def read_csv(filename):
     return df
 
 
+def write_json(filename, data):
+    """Writes JSON data to a file."""
+    with open(filename, "w") as f:
+        json.dump(data, f, indent=4)
+
+
 if __name__ == "__main__":
     filename = "data/books.csv"
     df = read_csv(filename)
+    books_data = df.to_dict(orient='records')
+    write_json("data/books.json", books_data)
